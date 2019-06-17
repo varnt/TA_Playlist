a283bac9e0026b26a4d3ba9d3b4bb8ffbf9ff6ce
Victor Arnt <35453021+varnt@users.noreply.github.com>
2019-06-16 22:55:30 -0300

    menu_completo

--- main.cpp
+++ main.cpp
@@ -5,15 +5,29 @@ using namespace std;
 
 int menu()
 {
+    int opção = 0;
     cout << "O que gostaria de fazer?" << endl;
     cout << "1- add musica em uma playlist" << endl;
     cout << "2- remover musica da playlist" << endl;
     cout << "3- add playlist" << endl;
-    cout << "4- " << endl;
+    cout << "4- imprimir playlist na tela" << endl;
+    cout << "5- tempo total e medio da playlist" << endl;
+    cout << "6- genero especifico" << endl;
+    cout << "7- ordenar por ordem alfabetica" << endl;
+    cout << "8- sair" << endl << endl;
+
+    cin >> opção;
+    return(opção);
 }
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    int opção = 0;
+    opção = menu();
+
+    switch
+        case
+
+
     return 0;
 }
